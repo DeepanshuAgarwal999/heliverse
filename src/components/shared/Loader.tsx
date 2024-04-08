acfe6d8c1dfb459e9b71b624092340b536664951
deepanshu agarwal <deepanshuagarwal9999@gmail.com>
2024-04-08 10:18:27 +0530

    fix loader

--- src/components/shared/Loader.tsx
+++ src/components/shared/Loader.tsx
@@ -8,7 +8,6 @@ const Loader = () => {
             <span
                 className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
             >Loading...</span>
-            <h1>Loading...</h1>
         </div>
     )
 }
